4451a38b5a49cafaa6f0a39dab30535d3b8ff678
Humberto <hlsp999@gmail.com>
2020-12-13 15:11:11 +0000

    Performance: Add cache system with redis to speed up returning page load speed

--- app/services/marvel_api.rb
+++ app/services/marvel_api.rb
@@ -1,22 +1,31 @@
 class MarvelApi
 
   HOST = "gateway.marvel.com"
+  PATH = "/v1/public/"
   PUBLIC_KEY = Rails.application.credentials.marvel_public_key
   PRIVATE_KEY = Rails.application.credentials.marvel_private_key
+  REDIS_TTL = 60 * 24 * 24
+
 
   attr_reader :response, :body, :data, :results
 
   def initialize(endpoint = '')
-    @endpoint = "/v1/public/" + endpoint
+    @endpoint = endpoint
     ts = Time.now.to_i.to_s
     hash_value = Digest::MD5.hexdigest ts + PRIVATE_KEY + PUBLIC_KEY
     @authentication_query_string = "ts=#{Time.now.to_i}&apikey=#{PUBLIC_KEY}&hash=#{hash_value}"
   end
 
   def request
-    @response ||= Net::HTTP.get_response(uri)
-    @body = JSON.parse(@response.body)
-    @data = @body["data"]
+    if cached?
+      @data = JSON.parse(cache)
+    else
+      @response ||= Net::HTTP.get_response(uri)
+      @body = JSON.parse(@response.body)
+      @data = @body["data"]
+      @results = @data["results"]
+      redis.set(redis_key, @data.to_json, ex: REDIS_TTL)
+    end
     @results = @data["results"]
   end
 
@@ -26,8 +35,25 @@ class MarvelApi
 
   private
     def uri
-      URI::HTTPS.build(host: HOST, path: @endpoint, query: @authentication_query_string + @query.to_s)
+      URI::HTTPS.build(host: HOST, path: PATH + @endpoint, query: @authentication_query_string + @query.to_s)
     end
 
+    def redis
+      begin
+        Redis.current
+      rescue
+        warn "redis FAILED!! check if redis server is up!"
+      end
+    end
 
+    def cached?
+      redis.get(redis_key)
+    end
+    alias_method :cache, :cached?
+
+    def redis_key
+      # Exclude marvel params and use the remaining to form the redis key
+      decode_query = URI.decode_www_form(uri.query)
+      @endpoint + decode_query.reject{|param| ["ts", "hash", "apikey"].include? param[0]}.join
+    end
 end
--- spec/services/marvel_api_spec.rb
+++ spec/services/marvel_api_spec.rb
@@ -17,37 +17,65 @@ RSpec.describe MarvelApi do
     end
   end
 
-  describe 'request' do
-    it 'gets a response' do
-      VCR.use_cassette 'marvel/comics' do
-        marvel_api.request
-        expect(marvel_api.response).to be_a Net::HTTPOK
+  context 'wihtout cache' do
+    before :each do
+      marvel_api.send(:redis).flushdb
+    end
+
+    describe 'request' do
+      it 'gets a response' do
+        VCR.use_cassette 'marvel/comics' do
+          marvel_api.request
+          expect(marvel_api.response).to be_a Net::HTTPOK
+        end
+      end
+    end
+
+    context 'after the request' do
+      before :each do
+        VCR.use_cassette 'marvel/comics' do
+          marvel_api.request
+        end
+      end
+
+      describe 'body' do
+        it 'has a body with meta information along with the data' do
+          expect(marvel_api.body).to include "code", "status", "copyright", "attributionText", "attributionHTML", "etag", "data"
+        end
+      end
+
+      describe 'data' do
+        it 'has the data' do
+          expect(marvel_api.data).to include "offset", "limit", "total", "count", "results"
+        end
+      end
+
+      describe 'results' do
+        it 'has the results' do
+          expect(marvel_api.results[0].keys).to include "id", "digitalId", "title", "creators", "characters"
+        end
       end
     end
   end
 
-  context 'after the request' do
-    before :each do
-      VCR.use_cassette 'marvel/comics' do
-        marvel_api.request
+  context 'with cache' do
+    context 'after the request' do
+      before :each do
+        VCR.use_cassette 'marvel/comics' do
+          marvel_api.request
+        end
       end
-    end
 
-    describe 'body' do
-      it 'has a body with meta information along with the data' do
-        expect(marvel_api.body).to include "code", "status", "copyright", "attributionText", "attributionHTML", "etag", "data"
+      describe 'data' do
+        it 'has the data' do
+          expect(marvel_api.data).to include "offset", "limit", "total", "count", "results"
+        end
       end
-    end
 
-    describe 'data' do
-      it 'has the data' do
-        expect(marvel_api.data).to include "offset", "limit", "total", "count", "results"
-      end
-    end
-
-    describe 'results' do
-      it 'has the results' do
-        expect(marvel_api.results[0].keys).to include "id", "digitalId", "title", "creators", "characters"
+      describe 'results' do
+        it 'has the results' do
+          expect(marvel_api.results[0].keys).to include "id", "digitalId", "title", "creators", "characters"
+        end
       end
     end
   end
@@ -57,6 +85,20 @@ RSpec.describe MarvelApi do
     it 'has a uri' do
       expect(marvel_api.send(:uri)).to be_a URI::HTTPS
       expect(marvel_api.send(:uri).request_uri).to eq "/v1/public/comics?ts=1607731322&apikey=07e3e205bebd46de31d15ee9a76d85c2&hash=92c26f3f2c82cd12ce27f137e0e7ee31"
+    end
+
+    it 'has a redis key excluding marvel params' do
+      marvel_api.query= "test=redis"
+      expect(marvel_api.send(:redis_key)).to eq "comicstestredis"
+    end
+
+    it 'has a checks if there is cached information within redis' do
+      marvel_api.send(:redis).flushdb
+      expect(marvel_api.send(:cached?)).to be_nil
+      marvel_api.request
+      expect(marvel_api.send(:cache)).to be_truthy
+      expect(marvel_api.send(:cached?)).to be_truthy
+      expect(marvel_api.results[0].keys).to include "id", "digitalId", "title", "issueNumber", "variantDescription"
     end
   end
 
