8e3b20f4aad743f433d9faa85499fd35c389a7ea
Humberto <hlsp999@gmail.com>
2020-12-13 12:57:55 +0000

    Feature: Add a cookie session expiring in 30 minutes to demonstrate comic favorites per user
    After first app server request the session is set to expire in 30 minutes allowing to flag comics as favorite.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_cookie
+
+  def set_cookie
+    unless cookies[:current_user]
+      cookies[:current_user] = { value: Time.current, expires: 30.minutes }
+    end
+  end
 end
