33385a753c1f65c853d28f464c8d13f5515ea372
Humberto <hlsp999@gmail.com>
2020-12-14 08:55:17 +0000

    Cosmetics: Wrap navigation buttons in 'a' tag

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,11 @@
 class CharactersController < ApplicationController
 
   def search
-    character = Character.new params["character_name"]
-    redirect_to comic_collections_path(characters: character.id)
+    if params["character_name"] != ""
+      character = Character.new params["character_name"]
+      redirect_to comic_collections_path(characters: character.id)
+    else
+      redirect_to comic_collections_path
+    end
   end
 end
